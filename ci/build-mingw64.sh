40749107e4a11e87f1b1cbf478bc43116f3a25ae
starchivore <briefcaseaod@yahoo.com>
2022-12-18 14:12:13 +0800

    Update build-mingw64.sh

--- ci/build-mingw64.sh
+++ ci/build-mingw64.sh
@@ -65,6 +65,28 @@ function gettar () {
     tar -xaf "$name"
 }
 
+## iconv
+if [ ! -e "$prefix_dir/lib/libiconv.dll.a" ]; then
+    ver=1.17
+    gettar "https://ftp.gnu.org/pub/gnu/libiconv/libiconv-${ver}.tar.gz"
+    builddir libiconv-${ver}
+    ../configure --host=$TARGET $commonflags
+    makeplusinstall
+    popd
+fi
+
+## zlib
+if [ ! -e "$prefix_dir/lib/libz.dll.a" ]; then
+    ver=1.2.12
+    gettar "https://zlib.net/fossils/zlib-${ver}.tar.gz"
+    pushd zlib-${ver}
+    make -fwin32/Makefile.gcc clean
+    make -fwin32/Makefile.gcc PREFIX=$TARGET- SHARED_MODE=1 \
+        DESTDIR="$prefix_dir" install \
+        BINARY_PATH=/bin INCLUDE_PATH=/include LIBRARY_PATH=/lib
+    popd
+fi
+
 ## ffmpeg
 if [ ! -e "$prefix_dir/lib/libavcodec.dll.a" ]; then
     [ -d ffmpeg ] || $gitclone https://github.com/FFmpeg/FFmpeg.git ffmpeg
@@ -77,6 +99,71 @@ if [ ! -e "$prefix_dir/lib/libavcodec.dll.a" ]; then
     popd
 fi
 
+## shaderc
+if [ ! -e "$prefix_dir/lib/libshaderc_shared.dll.a" ]; then
+    if [ ! -d shaderc ]; then
+        $gitclone https://github.com/google/shaderc.git
+        (cd shaderc && ./utils/git-sync-deps)
+    fi
+    builddir shaderc
+    cmake .. -DCMAKE_SYSTEM_NAME=Windows \
+        -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF \
+        -DSHADERC_SKIP_TESTS=ON -DCMAKE_INSTALL_PREFIX=/
+    makeplusinstall
+    popd
+fi
+
+## spirv-cross
+if [ ! -e "$prefix_dir/lib/libspirv-cross-c-shared.dll.a" ]; then
+    [ -d SPIRV-Cross ] || $gitclone https://github.com/KhronosGroup/SPIRV-Cross
+    builddir SPIRV-Cross
+    cmake .. -DCMAKE_SYSTEM_NAME=Windows \
+        -DSPIRV_CROSS_SHARED=ON -DSPIRV_CROSS_{CLI,STATIC}=OFF
+    makeplusinstall
+    popd
+fi
+
+## libplacebo
+if [ ! -e "$prefix_dir/lib/libplacebo.dll.a" ]; then
+    [ -d libplacebo ] || $gitclone https://code.videolan.org/videolan/libplacebo.git
+    builddir libplacebo
+    meson .. --cross-file "$prefix_dir/crossfile"
+    makeplusinstall
+    popd
+fi
+
+## freetype2
+if [ ! -e "$prefix_dir/lib/libfreetype.dll.a" ]; then
+    ver=2.12.1
+    gettar "https://download.savannah.gnu.org/releases/freetype/freetype-${ver}.tar.xz"
+    builddir freetype-${ver}
+    meson .. --cross-file "$prefix_dir/crossfile"
+    makeplusinstall
+    popd
+fi
+
+## fribidi
+if [ ! -e "$prefix_dir/lib/libfribidi.dll.a" ]; then
+    ver=1.0.12
+    gettar "https://github.com/fribidi/fribidi/releases/download/v${ver}/fribidi-${ver}.tar.xz"
+    builddir fribidi-${ver}
+    meson .. --cross-file "$prefix_dir/crossfile" \
+        -D{tests,docs}=false
+    makeplusinstall
+    popd
+fi
+
+## harfbuzz
+if [ ! -e "$prefix_dir/lib/libharfbuzz.dll.a" ]; then
+    ver=5.3.0
+    gettar "https://github.com/harfbuzz/harfbuzz/releases/download/${ver}/harfbuzz-${ver}.tar.xz"
+    builddir harfbuzz-${ver}
+    meson .. --cross-file "$prefix_dir/crossfile" \
+        -Dtests=disabled
+    makeplusinstall
+    popd
+fi
+
 ## libass
 if [ ! -e "$prefix_dir/lib/libass.dll.a" ]; then
     [ -d libass ] || $gitclone https://github.com/libass/libass.git
@@ -84,6 +171,20 @@ if [ ! -e "$prefix_dir/lib/libass.dll.a" ]; then
     [ -f ../configure ] || (cd .. && ./autogen.sh)
     ../configure --host=$TARGET $commonflags
     makeplusinstall
+    popd
+fi
+
+## luajit
+if [ ! -e "$prefix_dir/lib/libluajit-5.1.a" ]; then
+    ver=2.1.0-beta3
+    gettar "http://luajit.org/download/LuaJIT-${ver}.tar.gz"
+    pushd LuaJIT-${ver}
+    hostcc=cc
+    [[ "$TARGET" == "i686-"* ]] && hostcc="$hostcc -m32"
+    make TARGET_SYS=Windows clean
+    make TARGET_SYS=Windows HOST_CC="$hostcc" CROSS=$TARGET- \
+        BUILDMODE=static amalg
+    make DESTDIR="$prefix_dir" INSTALL_DEP= FILE_T=luajit.exe install
     popd
 fi
 
