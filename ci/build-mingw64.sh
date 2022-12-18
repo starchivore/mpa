885825eef7b781fdb32ba064d14a403a5ad7bb1c
starchivore <briefcaseaod@yahoo.com>
2022-12-18 08:03:22 +0800

    Update build-mingw64.sh

--- ci/build-mingw64.sh
+++ ci/build-mingw64.sh
@@ -99,6 +99,81 @@ if [ ! -e "$prefix_dir/lib/libavcodec.dll.a" ]; then
     popd
 fi
 
+## shaderc
+if [ ! -e "$prefix_dir/lib/libshaderc_shared.dll.a" ]; then
+    if [ ! -d shaderc ]; then
+        $gitclone https://github.com/google/shaderc.git
+        (cd shaderc && ./utils/git-sync-deps)
+    fi
+    builddir shaderc
+    cmake .. -DCMAKE_SYSTEM_NAME=Windows \
+        -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF \
+        -DSHADERC_SKIP_TESTS=ON -DCMAKE_INSTALL_PREFIX=/
+    makeplusinstall
+    popd
+fi
+
+## spirv-cross
+if [ ! -e "$prefix_dir/lib/libspirv-cross-c-shared.dll.a" ]; then
+    [ -d SPIRV-Cross ] || $gitclone https://github.com/KhronosGroup/SPIRV-Cross
+    builddir SPIRV-Cross
+    cmake .. -DCMAKE_SYSTEM_NAME=Windows \
+        -DSPIRV_CROSS_SHARED=ON -DSPIRV_CROSS_{CLI,STATIC}=OFF
+    makeplusinstall
+    popd
+fi
+
+## libplacebo
+if [ ! -e "$prefix_dir/lib/libplacebo.dll.a" ]; then
+    [ -d libplacebo ] || $gitclone https://code.videolan.org/videolan/libplacebo.git
+    builddir libplacebo
+    meson .. --cross-file "$prefix_dir/crossfile"
+    makeplusinstall
+    popd
+fi
+
+## freetype2
+if [ ! -e "$prefix_dir/lib/libfreetype.dll.a" ]; then
+    ver=2.12.1
+    gettar "https://download.savannah.gnu.org/releases/freetype/freetype-${ver}.tar.xz"
+    builddir freetype-${ver}
+    meson .. --cross-file "$prefix_dir/crossfile"
+    makeplusinstall
+    popd
+fi
+
+## fribidi
+if [ ! -e "$prefix_dir/lib/libfribidi.dll.a" ]; then
+    ver=1.0.12
+    gettar "https://github.com/fribidi/fribidi/releases/download/v${ver}/fribidi-${ver}.tar.xz"
+    builddir fribidi-${ver}
+    meson .. --cross-file "$prefix_dir/crossfile" \
+        -D{tests,docs}=false
+    makeplusinstall
+    popd
+fi
+
+## harfbuzz
+if [ ! -e "$prefix_dir/lib/libharfbuzz.dll.a" ]; then
+    ver=5.3.0
+    gettar "https://github.com/harfbuzz/harfbuzz/releases/download/${ver}/harfbuzz-${ver}.tar.xz"
+    builddir harfbuzz-${ver}
+    meson .. --cross-file "$prefix_dir/crossfile" \
+        -Dtests=disabled
+    makeplusinstall
+    popd
+fi
+
+## libass
+if [ ! -e "$prefix_dir/lib/libass.dll.a" ]; then
+    [ -d libass ] || $gitclone https://github.com/libass/libass.git
+    builddir libass
+    [ -f ../configure ] || (cd .. && ./autogen.sh)
+    ../configure --host=$TARGET $commonflags
+    makeplusinstall
+    popd
+fi
+
 ## luajit
 if [ ! -e "$prefix_dir/lib/libluajit-5.1.a" ]; then
     ver=2.1.0-beta3
@@ -125,11 +200,14 @@ rm -rf build
 if [ "$1" = "meson" ]; then
     meson setup build --cross-file "$prefix_dir/crossfile" \
         --buildtype debugoptimized \
-        -D{libmpv,tests}=false -Dlua=luajit \
+        -D{libmpv,tests}=true -Dlua=luajit \
+        -D{shaderc,spirv-cross,d3d11,libplacebo}=enabled
 
     ninja -C build --verbose
 elif [ "$1" = "waf" ]; then
-    PKG_CONFIG=pkg-config ./waf configure --lua=luajit
+    PKG_CONFIG=pkg-config ./waf configure \
+        --enable-libmpv-shared --lua=luajit \
+        --enable-{shaderc,spirv-cross,d3d11,libplacebo,tests}
 
     ./waf build --verbose
 fi
